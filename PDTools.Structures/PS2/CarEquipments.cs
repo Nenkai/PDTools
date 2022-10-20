db63b23db67492a58efeeab4e108877a480a80dc
Nenkai <Nenkai@users.noreply.github.com>
2022-10-20 18:34:03 +0200

    Add figured GT4 CarEquipments values

--- PDTools.Structures/PS2/CarEquipments.cs
+++ PDTools.Structures/PS2/CarEquipments.cs
@@ -43,59 +43,59 @@ namespace PDTools.Structures.PS2
         public DbCode NOS { get; set; }
         public DbCode Wing { get; set; }
         public DbCode SuperCharger { get; set; }
-        public short GearReverse { get; set; }
-        public short GearRatio1 { get; set; }
-        public short GearRatio2 { get; set; }
-        public short GearRatio3 { get; set; }
-        public short GearRatio4 { get; set; }
-        public short GearRatio5 { get; set; }
-        public short GearRatio6 { get; set; }
-        public short GearRatio7 { get; set; }
-        public short GearRatio8 { get; set; }
-        public short GearRatio9 { get; set; }
-        public short GearRatio10 { get; set; }
-        public short GearRatio11 { get; set; }
-        public short FinalGearRatio { get; set; }
-        public short MaxSpeed { get; set; }
-        public short LastFinalGearRatio { get; set; }
+        public ushort GearReverse { get; set; }
+        public ushort GearRatio1 { get; set; }
+        public ushort GearRatio2 { get; set; }
+        public ushort GearRatio3 { get; set; }
+        public ushort GearRatio4 { get; set; }
+        public ushort GearRatio5 { get; set; }
+        public ushort GearRatio6 { get; set; }
+        public ushort GearRatio7 { get; set; }
+        public ushort GearRatio8 { get; set; }
+        public ushort GearRatio9 { get; set; }
+        public ushort GearRatio10 { get; set; }
+        public ushort GearRatio11 { get; set; }
+        public ushort FinalGearRatio { get; set; }
+        public ushort MaxSpeed { get; set; }
+        public ushort LastFinalGearRatio { get; set; }
         public byte Param4WD { get; set; }
-        public byte FrontABS { get; set; }
-        public byte RearABS { get; set; }
+        public byte ABSLevelF { get; set; }
+        public byte ABSLevelR { get; set; }
         public byte SettingClF { get; set; }
         public byte SettingClR { get; set; }
-        public byte Boost1 { get; set; }
-        public byte PeakRPM1 { get; set; }
-        public byte Response1 { get; set; }
-        public byte Boost2 { get; set; }
-        public byte PeakRPM2 { get; set; }
-        public byte Response2 { get; set; }
-        public byte field_136 { get; set; }
-        public byte field_137 { get; set; }
+        public byte Turbo_Boost1 { get; set; }
+        public byte Turbo_PeakRPM1 { get; set; }
+        public byte Turbo_Response1 { get; set; }
+        public byte Turbo_Boost2 { get; set; }
+        public byte Turbo_PeakRPM2 { get; set; }
+        public byte Turbo_Response2 { get; set; }
+        public byte Susp_FrontCamber { get; set; }
+        public byte Susp_RearCamber { get; set; }
         public byte field_138 { get; set; }
         public short Susp_RideHeightF { get; set; }
         public short Susp_RideHeightR { get; set; }
-        public byte FrontToe { get; set; }
-        public byte RearToe { get; set; }
-        public byte FrontSpringRate { get; set; }
-        public byte RearSpringRate { get; set; }
+        public byte Susp_FrontToe { get; set; }
+        public byte Susp_RearToe { get; set; }
+        public byte Susp_FrontSpringRate { get; set; }
+        public byte Susp_RearSpringRate { get; set; }
         public byte leverRatioF { get; set; }
         public byte leverRatioR { get; set; }
-        public byte FrontDamperF1B { get; set; }
-        public byte FrontDamperF2B { get; set; }
-        public byte FrontDamperF1R { get; set; }
-        public byte FrontDamperF2R { get; set; }
-        public byte RearDamperF1B { get; set; }
-        public byte RearDamperF2B { get; set; }
-        public byte RearDamperF1R { get; set; }
-        public byte RearDamperF2R { get; set; }
-        public byte FrontStabilizer { get; set; }
-        public byte RearStabilizer { get; set; }
+        public byte Susp_FrontDamperF1B { get; set; }
+        public byte Susp_FrontDamperF2B { get; set; }
+        public byte Susp_FrontDamperF1R { get; set; }
+        public byte Susp_FrontDamperF2R { get; set; }
+        public byte Susp_RearDamperF1B { get; set; }
+        public byte Susp_RearDamperF2B { get; set; }
+        public byte Susp_RearDamperF1R { get; set; }
+        public byte Susp_RearDamperF2R { get; set; }
+        public byte Susp_FrontStabilizer { get; set; }
+        public byte Susp_RearStabilizer { get; set; }
         public byte FrontLSDparam { get; set; }
         public byte FrontLSDparam2 { get; set; }
         public byte FrontLSDparam3 { get; set; }
         public byte RearLSDparam { get; set; }
         public byte RearLSDparam2 { get; set; }
-        public byte RearLSDpara3 { get; set; }
+        public byte RearLSDparam3 { get; set; }
         public byte TCSC_Value { get; set; }
         public byte ASCC_VSC1 { get; set; }
         public byte ASCC_VSC2 { get; set; }
@@ -108,27 +108,20 @@ namespace PDTools.Structures.PS2
         public byte unk3 { get; set; }
         public byte unk4 { get; set; }
         public byte unk5 { get; set; }
-        public byte unk6  { get; set; }
-        public byte field_15D { get; set; }
-        public short word15E { get; set; }
-        public byte byte160 { get; set; }
-        public byte byte161 { get; set; }
-        public byte byte162 { get; set; }
+        public byte WeightMultiplier  { get; set; }
+        public byte PowerMultiplier { get; set; }
+        public ushort UnkTunedCarValue { get; set; }
+        public byte IsStrange { get; set; }
+        public byte NOSTorqueVolume { get; set; }
+        public byte TireSize_StiffnessCategory { get; set; }
         public byte byte163 { get; set; }
-        public byte byte164 { get; set; }
-        public byte byte165 { get; set; }
-        public byte byte166 { get; set; }
-        public byte unk7 { get; set; }
-        public byte unk8 { get; set; }
+        public byte Gear_WeightRoller { get; set; }
+        public byte Susp_FrontPreLoadLevel { get; set; }
+        public byte Susp_RearPreLoadLevel { get; set; }
+        public byte Susp_FrontSpringRateLevel { get; set; }
+        public byte Susp_RearSpringRateLevel { get; set; }
         public byte byte169 { get; set; }
-        public byte unk9 { get; set; }
-        public byte byte16B { get; set; }
-        public byte byte16C { get; set; }
-        public byte byte16D { get; set; }
-        public byte byte16E { get; set; }
-        public byte field_16F { get; set; }
-        public byte field_170 { get; set; }
-        public byte field_171 { get; set; }
+
         public byte[] Unk_GT4OData { get; set; }
 
         public void Unpack(ref SpanReader sr, bool gt4o = false)
@@ -168,59 +161,59 @@ namespace PDTools.Structures.PS2
             NOS = DbCode.Unpack(ref sr);
             Wing = DbCode.Unpack(ref sr);
             SuperCharger = DbCode.Unpack(ref sr);
-            GearReverse = sr.ReadInt16();
-            GearRatio1 = sr.ReadInt16();
-            GearRatio2 = sr.ReadInt16();
-            GearRatio3 = sr.ReadInt16();
-            GearRatio4 = sr.ReadInt16();
-            GearRatio5 = sr.ReadInt16();
-            GearRatio6 = sr.ReadInt16();
-            GearRatio7 = sr.ReadInt16();
-            GearRatio8 = sr.ReadInt16();
-            GearRatio9 = sr.ReadInt16();
-            GearRatio10 = sr.ReadInt16();
-            GearRatio11 = sr.ReadInt16();
-            FinalGearRatio = sr.ReadInt16();
-            MaxSpeed = sr.ReadInt16();
-            LastFinalGearRatio = sr.ReadInt16();
+            GearReverse = sr.ReadUInt16();
+            GearRatio1 = sr.ReadUInt16();
+            GearRatio2 = sr.ReadUInt16();
+            GearRatio3 = sr.ReadUInt16();
+            GearRatio4 = sr.ReadUInt16();
+            GearRatio5 = sr.ReadUInt16();
+            GearRatio6 = sr.ReadUInt16();
+            GearRatio7 = sr.ReadUInt16();
+            GearRatio8 = sr.ReadUInt16();
+            GearRatio9 = sr.ReadUInt16();
+            GearRatio10 = sr.ReadUInt16();
+            GearRatio11 = sr.ReadUInt16();
+            FinalGearRatio = sr.ReadUInt16();
+            MaxSpeed = sr.ReadUInt16();
+            LastFinalGearRatio = sr.ReadUInt16();
             Param4WD = sr.ReadByte();
-            FrontABS = sr.ReadByte();
-            RearABS = sr.ReadByte();
+            ABSLevelF = sr.ReadByte();
+            ABSLevelR = sr.ReadByte();
             SettingClF = sr.ReadByte();
             SettingClR = sr.ReadByte();
-            Boost1 = sr.ReadByte();
-            PeakRPM1 = sr.ReadByte();
-            Response1 = sr.ReadByte();
-            Boost2 = sr.ReadByte();
-            PeakRPM2 = sr.ReadByte();
-            Response2 = sr.ReadByte();
-            field_136 = sr.ReadByte();
-            field_137 = sr.ReadByte();
+            Turbo_Boost1 = sr.ReadByte();
+            Turbo_PeakRPM1 = sr.ReadByte();
+            Turbo_Response1 = sr.ReadByte();
+            Turbo_Boost2 = sr.ReadByte();
+            Turbo_PeakRPM2 = sr.ReadByte();
+            Turbo_Response2 = sr.ReadByte();
+            Susp_FrontCamber = sr.ReadByte();
+            Susp_RearCamber = sr.ReadByte();
             field_138 = sr.ReadByte();
             Susp_RideHeightF = sr.ReadInt16();
             Susp_RideHeightR = sr.ReadInt16();
-            FrontToe = sr.ReadByte();
-            RearToe = sr.ReadByte();
-            FrontSpringRate = sr.ReadByte();
-            RearSpringRate = sr.ReadByte();
+            Susp_FrontToe = sr.ReadByte();
+            Susp_RearToe = sr.ReadByte();
+            Susp_FrontSpringRate = sr.ReadByte();
+            Susp_RearSpringRate = sr.ReadByte();
             leverRatioF = sr.ReadByte();
             leverRatioR = sr.ReadByte();
-            FrontDamperF1B = sr.ReadByte();
-            FrontDamperF2B = sr.ReadByte();
-            FrontDamperF1R = sr.ReadByte();
-            FrontDamperF2R = sr.ReadByte();
-            RearDamperF1B = sr.ReadByte();
-            RearDamperF2B = sr.ReadByte();
-            RearDamperF1R = sr.ReadByte();
-            RearDamperF2R = sr.ReadByte();
-            FrontStabilizer = sr.ReadByte();
-            RearStabilizer = sr.ReadByte();
+            Susp_FrontDamperF1B = sr.ReadByte();
+            Susp_FrontDamperF2B = sr.ReadByte();
+            Susp_FrontDamperF1R = sr.ReadByte();
+            Susp_FrontDamperF2R = sr.ReadByte();
+            Susp_RearDamperF1B = sr.ReadByte();
+            Susp_RearDamperF2B = sr.ReadByte();
+            Susp_RearDamperF1R = sr.ReadByte();
+            Susp_RearDamperF2R = sr.ReadByte();
+            Susp_FrontStabilizer = sr.ReadByte();
+            Susp_RearStabilizer = sr.ReadByte();
             FrontLSDparam = sr.ReadByte();
             FrontLSDparam2 = sr.ReadByte();
             FrontLSDparam3 = sr.ReadByte();
             RearLSDparam = sr.ReadByte();
             RearLSDparam2 = sr.ReadByte();
-            RearLSDpara3 = sr.ReadByte();
+            RearLSDparam3 = sr.ReadByte();
             TCSC_Value = sr.ReadByte();
             ASCC_VSC1 = sr.ReadByte();
             ASCC_VSC2 = sr.ReadByte();
@@ -233,27 +226,21 @@ namespace PDTools.Structures.PS2
             unk3 = sr.ReadByte();
             unk4 = sr.ReadByte();
             unk5 = sr.ReadByte();
-            unk6 = sr.ReadByte();
-            field_15D = sr.ReadByte();
-            word15E = sr.ReadInt16();
-            byte160 = sr.ReadByte();
-            byte161 = sr.ReadByte();
-            byte162 = sr.ReadByte();
+            WeightMultiplier = sr.ReadByte();
+            PowerMultiplier = sr.ReadByte();
+            UnkTunedCarValue = sr.ReadUInt16();
+            IsStrange = sr.ReadByte();
+            NOSTorqueVolume = sr.ReadByte();
+            TireSize_StiffnessCategory = sr.ReadByte();
             byte163 = sr.ReadByte();
-            byte164 = sr.ReadByte();
-            byte165 = sr.ReadByte();
-            byte166 = sr.ReadByte();
-            unk7 = sr.ReadByte();
-            unk8 = sr.ReadByte();
+            Gear_WeightRoller = sr.ReadByte();
+            Susp_FrontPreLoadLevel = sr.ReadByte();
+            Susp_RearPreLoadLevel = sr.ReadByte();
+            Susp_FrontSpringRateLevel = sr.ReadByte();
+            Susp_RearSpringRateLevel = sr.ReadByte();
             byte169 = sr.ReadByte();
-            unk9 = sr.ReadByte();
-            byte16B = sr.ReadByte();
-            byte16C = sr.ReadByte();
-            byte16D = sr.ReadByte();
-            byte16E = sr.ReadByte();
-            field_16F = sr.ReadByte();
-            field_170 = sr.ReadByte();
-            field_171 = sr.ReadByte();
+
+            sr.Position += 8; // Empty bytes
 
             if (gt4o)
                 Unk_GT4OData = sr.ReadBytes(0x18);
@@ -296,59 +283,59 @@ namespace PDTools.Structures.PS2
             NOS.Pack(ref sw);
             Wing.Pack(ref sw);
             SuperCharger.Pack(ref sw);
-            sw.WriteInt16(GearReverse);
-            sw.WriteInt16(GearRatio1);
-            sw.WriteInt16(GearRatio2);
-            sw.WriteInt16(GearRatio3);
-            sw.WriteInt16(GearRatio4);
-            sw.WriteInt16(GearRatio5);
-            sw.WriteInt16(GearRatio6);
-            sw.WriteInt16(GearRatio7);
-            sw.WriteInt16(GearRatio8);
-            sw.WriteInt16(GearRatio9);
-            sw.WriteInt16(GearRatio10);
-            sw.WriteInt16(GearRatio11);
-            sw.WriteInt16(FinalGearRatio);
-            sw.WriteInt16(MaxSpeed);
-            sw.WriteInt16(LastFinalGearRatio);
+            sw.WriteUInt16(GearReverse);
+            sw.WriteUInt16(GearRatio1);
+            sw.WriteUInt16(GearRatio2);
+            sw.WriteUInt16(GearRatio3);
+            sw.WriteUInt16(GearRatio4);
+            sw.WriteUInt16(GearRatio5);
+            sw.WriteUInt16(GearRatio6);
+            sw.WriteUInt16(GearRatio7);
+            sw.WriteUInt16(GearRatio8);
+            sw.WriteUInt16(GearRatio9);
+            sw.WriteUInt16(GearRatio10);
+            sw.WriteUInt16(GearRatio11);
+            sw.WriteUInt16(FinalGearRatio);
+            sw.WriteUInt16(MaxSpeed);
+            sw.WriteUInt16(LastFinalGearRatio);
             sw.WriteByte(Param4WD);
-            sw.WriteByte(FrontABS);
-            sw.WriteByte(RearABS);
+            sw.WriteByte(ABSLevelF);
+            sw.WriteByte(ABSLevelR);
             sw.WriteByte(SettingClF);
             sw.WriteByte(SettingClR);
-            sw.WriteByte(Boost1);
-            sw.WriteByte(PeakRPM1);
-            sw.WriteByte(Response1);
-            sw.WriteByte(Boost2);
-            sw.WriteByte(PeakRPM2);
-            sw.WriteByte(Response2);
-            sw.WriteByte(field_136);
-            sw.WriteByte(field_137);
+            sw.WriteByte(Turbo_Boost1);
+            sw.WriteByte(Turbo_PeakRPM1);
+            sw.WriteByte(Turbo_Response1);
+            sw.WriteByte(Turbo_Boost2);
+            sw.WriteByte(Turbo_PeakRPM2);
+            sw.WriteByte(Turbo_Response2);
+            sw.WriteByte(Susp_FrontCamber);
+            sw.WriteByte(Susp_RearCamber);
             sw.WriteByte(field_138);
             sw.WriteInt16(Susp_RideHeightF);
             sw.WriteInt16(Susp_RideHeightR);
-            sw.WriteByte(FrontToe);
-            sw.WriteByte(RearToe);
-            sw.WriteByte(FrontSpringRate);
-            sw.WriteByte(RearSpringRate);
+            sw.WriteByte(Susp_FrontToe);
+            sw.WriteByte(Susp_RearToe);
+            sw.WriteByte(Susp_FrontSpringRate);
+            sw.WriteByte(Susp_RearSpringRate);
             sw.WriteByte(leverRatioF);
             sw.WriteByte(leverRatioR);
-            sw.WriteByte(FrontDamperF1B);
-            sw.WriteByte(FrontDamperF2B);
-            sw.WriteByte(FrontDamperF1R);
-            sw.WriteByte(FrontDamperF2R);
-            sw.WriteByte(RearDamperF1B);
-            sw.WriteByte(RearDamperF2B);
-            sw.WriteByte(RearDamperF1R);
-            sw.WriteByte(RearDamperF2R);
-            sw.WriteByte(FrontStabilizer);
-            sw.WriteByte(RearStabilizer);
+            sw.WriteByte(Susp_FrontDamperF1B);
+            sw.WriteByte(Susp_FrontDamperF2B);
+            sw.WriteByte(Susp_FrontDamperF1R);
+            sw.WriteByte(Susp_FrontDamperF2R);
+            sw.WriteByte(Susp_RearDamperF1B);
+            sw.WriteByte(Susp_RearDamperF2B);
+            sw.WriteByte(Susp_RearDamperF1R);
+            sw.WriteByte(Susp_RearDamperF2R);
+            sw.WriteByte(Susp_FrontStabilizer);
+            sw.WriteByte(Susp_RearStabilizer);
             sw.WriteByte(FrontLSDparam);
             sw.WriteByte(FrontLSDparam2);
             sw.WriteByte(FrontLSDparam3);
             sw.WriteByte(RearLSDparam);
             sw.WriteByte(RearLSDparam2);
-            sw.WriteByte(RearLSDpara3);
+            sw.WriteByte(RearLSDparam3);
             sw.WriteByte(TCSC_Value);
             sw.WriteByte(ASCC_VSC1);
             sw.WriteByte(ASCC_VSC2);
@@ -361,27 +348,21 @@ namespace PDTools.Structures.PS2
             sw.WriteByte(unk3);
             sw.WriteByte(unk4);
             sw.WriteByte(unk5);
-            sw.WriteByte(unk6);
-            sw.WriteByte(field_15D);
-            sw.WriteInt16(word15E);
-            sw.WriteByte(byte160);
-            sw.WriteByte(byte161);
-            sw.WriteByte(byte162);
+            sw.WriteByte(WeightMultiplier);
+            sw.WriteByte(PowerMultiplier);
+            sw.WriteUInt16(UnkTunedCarValue);
+            sw.WriteByte(IsStrange);
+            sw.WriteByte(NOSTorqueVolume);
+            sw.WriteByte(TireSize_StiffnessCategory);
             sw.WriteByte(byte163);
-            sw.WriteByte(byte164);
-            sw.WriteByte(byte165);
-            sw.WriteByte(byte166);
-            sw.WriteByte(unk7);
-            sw.WriteByte(unk8);
+            sw.WriteByte(Gear_WeightRoller);
+            sw.WriteByte(Susp_FrontPreLoadLevel);
+            sw.WriteByte(Susp_RearPreLoadLevel);
+            sw.WriteByte(Susp_FrontSpringRateLevel);
+            sw.WriteByte(Susp_RearSpringRateLevel);
             sw.WriteByte(byte169);
-            sw.WriteByte(unk9);
-            sw.WriteByte(byte16B);
-            sw.WriteByte(byte16C);
-            sw.WriteByte(byte16D);
-            sw.WriteByte(byte16E);
-            sw.WriteByte(field_16F);
-            sw.WriteByte(field_170);
-            sw.WriteByte(field_171);
+
+            sw.Position += 8;
 
             if (gt4o)
                 sw.WriteBytes(Unk_GT4OData);
